89788e4c8e9e4dc8e97ee3a5c6fb69ae5469cc18
Jacob Caniparoli <jcanipar@gmail.com>
2017-12-05 01:23:55 -0800

    Cleaned up the construction of the fleets a bit, removed extraneous comments

--- simulation.js
+++ simulation.js
@@ -1,52 +1,29 @@
 $( document ).ready(function() {
-  // Handler for .ready() called.
+  //Attaches the doSim function to the submit button on a click
   $('#simulateBtn').click(doSim);
 });
 
 function doSim() {
   
-  var fleet1 = new fleetCreate(2,9,1,9,0,8,3,6,1,5,0,3,2,6,0,8);
-  var fleet2 = new fleetCreate(0,9,0,9,0,8,2,6,3,5,0,3,2,6,0,8);
+  var fleet1 = new fleetCreate($('#fighterOne').val(), 9,
+                              $('#carrierOne').val(), 9,
+                              $('#destroyerOne').val(), 9,
+                              $('#cruiserOne').val(), 7,
+                              $('#dreddieOne').val(), 5,
+                              $('#warSunOne').val(), 3,
+                              $('#cannonOne').val(), 6,
+                              $('#infantryOne').val(), 9);
 
-  fleet1.fight = $('#fighterOne').val();  
-  fleet1.fightHit = 9;
-  fleet1.car = $('#carrierOne').val();
-  fleet1.carHit = 9;
-  fleet1.des = $('#destroyerOne').val();
-  fleet1.desHit = 9;
-  fleet1.cru = $('#cruiserOne').val();
-  fleet1.cruHit = 7;
-  fleet1.dred = $('#dreddieOne').val();
-  fleet1.dredHit = 5;
-  fleet1.sun = $('#warSunOne').val();
-  fleet1.sunHit = 3;
-  fleet1.pds = $('#cannonOne').val();
-  fleet1.pdsHit = 6;
-  fleet1.gf = $('#infantryOne').val();
-  fleet1.gfHit = 9;
+  var fleet2 = new fleetCreate($('#fighterTwo').val(), 9,
+                              $('#carrierTwo').val(), 9,
+                              $('#destroyerTwo').val(), 9,
+                              $('#cruiserTwo').val(), 7,
+                              $('#dreddieTwo').val(), 5,
+                              $('#warSunTwo').val(), 3,
+                              $('#cannonTwo').val(), 6,
+                              $('#infantryTwo').val(), 9);
 
-  //alert(fleetPrint(fleet1));
   alert(fleet1.print());
-
-
-  fleet2.fight = $('#fighterTwo').val();  
-  fleet2.fightHit = 9;
-  fleet2.car = $('#carrierTwo').val();
-  fleet2.carHit = 9;
-  fleet2.des = $('#destroyerTwo').val();
-  fleet2.desHit = 9;
-  fleet2.cru = $('#cruiserTwo').val();
-  fleet2.cruHit = 7;
-  fleet2.dred = $('#dreddieTwo').val();
-  fleet2.dredHit = 5;
-  fleet2.sun = $('#warSunTwo').val();
-  fleet2.sunHit = 3;
-  fleet2.pds = $('#cannonTwo').val();
-  fleet2.pdsHit = 6;
-  fleet2.gf = $('#infantryTwo').val();
-  fleet2.gfHit = 9;
-
-  //alert(fleetPrint(fleet2));
   alert(fleet2.print());
 
   var fleet1wins = 0;
@@ -93,6 +70,8 @@ function fleetCreate(fighter, fighterHit, carrier, carrierHit, destroyer, destro
   this.gf = infantry
   this.gfHit = infantryHit
 
+  //returns a string containing all of the fleet information - good for debugging
+  //this is a javascript thing I only kind of understand
   this.print = function () {
       output = "Fighters: " + this.fight + " Combat: " + this.fightHit + "\n";
       output += "Carriers: " + this.car + " Combat: " + this.carHit + "\n";
@@ -106,19 +85,6 @@ function fleetCreate(fighter, fighterHit, carrier, carrierHit, destroyer, destro
   };
 }
 
-
-
-function fleetPrint(fleet) {
-  output = "Fighters: " + fleet.fight + " Combat: " + fleet.fightHit + "\n";
-  output += "Carriers: " + fleet.car + " Combat: " + fleet.carHit + "\n";
-  output += "Destroyers: " + fleet.des + " Combat: " + fleet.desHit + "\n";
-  output += "Cruisers: " + fleet.cru + " Combat: " + fleet.cruHit + "\n";
-  output += "Dreddies: " + fleet.dred + " Combat: " + fleet.dredHit + "\n";
-  output += "War Sun: " + fleet.sun + " Combat: " + fleet.sunHit + "\n";
-  output += "PDS: " + fleet.pds + " Combat: " + fleet.pdsHit + "\n";
-  output += "Infantry: " + fleet.gf + " Combat: " + fleet.gfHit + "\n";
-  return output
-}
 
 //fleetSum counts up the number of ships in each fleet
 function fleetSum(fleet){
@@ -245,69 +211,3 @@ function fleetSim(fleet1, fleet2){
 
 }
 
-
-
-//
-//var fleet1 = new fleetCreate(2,9,1,9,0,8,3,6,1,5,0,3,2,6,0,8);
-//console.log(fleet1.fight);
-//fleet1.print();
-//var fleet2 = new fleetCreate(0,9,0,9,0,8,2,6,3,5,0,3,2,6,0,8);
-//
-//
-////This entire bit is for easy testing
-//fleet1.fight = 8;
-//fleet1.fightHit = 9;
-//fleet1.car = 1;
-//fleet1.carHit = 9;
-//fleet1.des = 2;
-//fleet1.desHit = 9;
-//fleet1.cru = 3;
-//fleet1.cruHit = 7;
-//fleet1.dred = 1;
-//fleet1.dredHit = 5;
-//fleet1.sun = 2;
-//fleet1.sunHit = 3;
-//fleet1.pds = 2;
-//fleet1.pdsHit = 6;
-//fleet1.gf = 0;
-//fleet1.gfHit = 9;
-//
-//
-//fleet2.fight = 6;
-//fleet2.fightHit = 9;
-//fleet2.car = 1;
-//fleet2.carHit = 9;
-//fleet2.des = 2;
-//fleet2.desHit = 9;
-//fleet2.cru = 3;
-//fleet2.cruHit = 7;
-//fleet2.dred = 1;
-//fleet2.dredHit = 5;
-//fleet2.sun = 2;
-//fleet2.sunHit = 3;
-//fleet2.pds = 2;
-//fleet2.pdsHit = 6;
-//fleet2.gf = 0;
-//fleet2.gfHit = 9;
-//
-//
-//
-//
-//var fleet1wins = 0;
-//var fleet2wins = 0;
-//for (var j = 0; j < 1000; j++){ //loops through the simulaion 1000 times figuring our who wins
-//
-  //var winner = 0;
-  //fleet1f = Object.assign({},fleet1);
-  //fleet2f = Object.assign({},fleet2);
-  //winner = fleetSim(fleet1f, fleet2f);
-  //if(winner == 1){
-    //fleet1wins++;
-  //} else {fleet2wins++}
-//
-//}
-//
-//console.log(fleet1wins);
-//$("#team1").text(fleet1wins);
-//console.log(fleet2wins);
-//$("#team2").text(fleet2wins);
